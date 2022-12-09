54003469ebe3d86a234d01d8132a9fa0b0aba949
Callum <84125856+callum-cheshire@users.noreply.github.com>
2022-12-09 16:11:46 +0000

    re enabled listen

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, function () {
-//     console.log(`CORS-enabled web server listening on port ${PORT}`);
-// });
+app.listen(PORT, function () {
+    console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
